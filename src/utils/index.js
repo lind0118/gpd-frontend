f497a6b762c97f093d1bf92d6448f4f2eb0abc20
christianagalindo7 <christianagalindo7@gmail.com>
2024-08-01 08:45:14 +0900

    Added new behavior and functionalities (#12)
    * Updated secretary page and created insert logic
    
    * Updated activity component (#8)
    
    - Activvity component now splitted by selector components
    - Status changer component created
    - Organized funtion handlers in activity component
    
    * Updated selector components (#10)
    
    * Updated activity component
    
    - Activvity component now splitted by selector components
    - Status changer component created
    - Organized funtion handlers in activity component
    
    * Updated selectors
    
    - Updated AcademicSelector
    - Fixed bug in GroupSelector
    
    * Updated secretary page (#11)
    
    * Updated secretary page
    
    - Added error modal on fetch error in /secretary and /index
    - Modified socket events name
    - Updated transactions.js new supabase transaccions added
    - sumHoras moved to /utils
    - singlePromiseResolver added to /utils
    - AcademicTemplateForm added to components
    
    * Many updates in secretary page
    
    - Table in /secretary is no longer a compact table
    - Created /plantilla/[id]/index.js page with SSG + ISR for template by id (dynamic route)
    - Changed ISR revalidate in /index from 10 seconds to 5 seconds
    - Updated title in /_document.js Head
    - Added setTemplateStatus in /models/transactions.js
    - Added behavior for "correction" status to ChangeStatus component
    - Added validation for template id in AcademicTemplateForm

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,12 @@
-import { createServerClient } from "@supabase/ssr";
 import { createClient } from "@supabase/supabase-js";
+
+export const promiseResolver = async (promiseList) => {
+    const results = await Promise.allSettled(promiseList)
+    const data = results.map(r => r.value)
+    return data
+}
+
+export const singlePromiseResolver = async (promise) =>  await promise
 
 export const puestos = [
     'Profesor de Tiempo Completo Titular "A"',
@@ -21,11 +28,10 @@ export const titulos = [
 
 export const distribucionActividades = [
     'Docencia',
-    'Atención a estudiantes',
-    'Atención personalizada',
-    'Gestión Institucional',
-    'Desarrollo de material',
-    'LIIAD'
+    'LIIAD',
+    'Tutorías',
+    'Gestión',
+    'Estadía técnica',
 ]
 
 export const modalidades = [
@@ -36,13 +42,13 @@ export const modalidades = [
     'Ingeniería Escolarizada',
     'Ingeniería Mixta',
 ]
-export const defaultActivity = {    
+export const defaultActivity = {
     id: crypto.randomUUID(),
-    pe: {
-        siglas: "",
-        descripcion: "",
-    },
+    pe: null,
     distribucion_actividades: "",
+    tipo_gestion: "",
+    tipo_estadia: "",
+    numero_estudiantes: 0,
     nombre_actividades: "",
     grados_grupos: [],
     horas_semanales: 0,
@@ -54,6 +60,8 @@ export const defaultRecord = {
     nombre: "",
     sexo: "",
     puesto: "",
+    anio: new Date().getFullYear(),
+    periodo: "",
     actividades: [
         defaultActivity
     ],
@@ -84,6 +92,23 @@ export const generatePeriods = (year, ordinario) => {
 }
 
 export const checkEmptyStringOption = (string) => string === "" ? [] : [string]
+
+export const sumHoras = (activities) => {
+    if (activities?.length){
+        return activities.map(e => e.subtotal_clasificacion).reduce((p, c) => p + c, 0)
+    }
+    return 0
+}
+
+export const generateTemplateObject = (record) => {
+    const plantilla = Object.fromEntries(Object.entries(record).filter(([k, v]) => {
+        if (k != 'actividades') {
+            return true
+        }
+    })
+    )
+    return plantilla
+}
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
